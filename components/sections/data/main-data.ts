2928a932b913bbabca0c75e32c89ce3512dc3edb
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-10-29 15:37:36 -0600

    basic version

---
+++ components/sections/data/main-data.ts
@@ -0,0 +1,353 @@
+export const quinceMainData = {
+  hero: {
+    name: "Graciela & Francisco",
+    subtitle: "¡Nuestra Boda!",
+    backgroundImage: "/images/g&f-01.jpg",
+    quote:
+      "La vida es un viaje mágico, y hoy celebro un capítulo especial lleno de sueños y esperanza.",
+    backgroundCarrouselImages: [
+      "/images/g&f-01.jpg",
+      "/images/g&f-02.jpg",
+      "/images/g&f-03.jpg",
+      "/images/g&f-04.jpg",
+      "/images/g&f-05.jpg",
+      "/images/g&f-06.jpg",
+      "/images/g&f-07.jpg",
+      "/images/g&f-08.jpg",
+    ],
+  },
+  welcomeSection: {
+    message: ` Con mucha alegría y emoción, te invitamos a celebrar nuestra Boda. 
+      Será un día lleno de magia, amor y momentos inolvidables. 
+      ¡Esperamos contar con tu presencia para hacer de este día algo realmente especial!`,
+    backgroundImage: "/images/orquideas1.jpg",
+  },
+  event: {
+    celebrant: "Graciela & Francisco",
+    novios: {
+      bride: "Graciela Castañeda Rosales",
+      groom: "Francisco Martínez Ríos",
+    },
+    // Justo Castañeda vanda y celia rosales medina
+    brideParents: {
+      father: "Justo Castañeda Vanda",
+      mother: "Celia Rosales Medina",
+    },
+    groomParents: {
+      father: "José Martínez",
+      mother: "Socorro Ríos",
+    },
+    //Papás Andres Martínez y Maria de Jesús Sanchez
+    parents: {
+      father: "Andres Martínez",
+      mother: "Maria de Jesús Sanchez",
+      message: ` Ustedes han sido nuestro primer ejemplo de amor y compromiso. 
+      Gracias por guiarnos con su sabiduría y por mostrarnos el camino del amor verdadero. 
+      Hoy, al unir nuestras vidas, llevamos con orgullo los valores y enseñanzas que nos han dado. 
+      ¡Los amamos profundamente!
+      `,
+      backgroundImage: "/images/orquideas4.jpg",
+    },
+    // padrinos luciana Martinez y atanasio  Adame
+    godparents: {
+      godfather: "Atanasio Adame",
+      godmother: "Luciana Martínez",
+    },
+    date: {
+      full: "Sábado 17 de Enero 2026",
+      isoDate: "2026-01-17T13:00:00",
+      day: "Sábado",
+      dayNumber: "17",
+      month: "Enero",
+      year: "2026",
+      date: "17 de Enero 2026",
+      mensaje1: "¡La cuenta regresiva ha comenzado!",
+      mensaje2: "TAN SOLO FALTAN",
+      backgroundCarrouselImages: [
+        "/images/boda5.jpeg",
+        "/images/boda6.jpeg",
+        "/images/boda7.jpeg",
+        "/images/boda8.jpeg",
+      ],
+    },
+    //Misa a la 1pm parroquia Santa fe de Guadalupe, la Sauceda Guanajuato.
+    ceremony: {
+      time: "13:00 hrs.",
+      venue: "Parroquia de San Marcos Evangelista",
+      address: "Alfonso Esparza Oteo 403, San Marcos, 37410 León de los Aldama, Gto.",
+      type: "Misa de Acción de Gracias",
+      ubiLink: "https://maps.app.goo.gl/VyLtPikVowZV4HJB8",
+      ceremonyImage: "/images/boda8.jpeg",
+    },
+    party: {
+      time: "14:00 hrs.",
+      venue: "C. 21 de Marzo Sur 1603",
+      address: "C. 21 de Marzo Sur 1603, San Marcos, 37410 León de los Aldama, Gto.",
+      type: "Recepción",
+      ubiLink: "https://maps.app.goo.gl/F9qPow6WpvyhB2wa7",
+    },
+    dressCode: "Formal - Blanco solo la novia -",
+    restrictions: "",
+  },
+  timeline: {
+    title: "Itinerario del Evento",
+    timelineImage: "/images/boda10.jpeg",
+    mensaje: `Cada momento de este día especial ha sido cuidadosamente planeado 
+    para crear recuerdos inolvidables. 
+    Desde la ceremonia hasta la celebración, cada detalle refleja el amor y 
+    la alegría que compartimos. ¡Espero que disfrutes cada instante tanto como nosotros!`,
+    images: [
+      "/images/boda9.jpeg",
+      "/images/boda10.jpeg",
+      "/images/boda1.jpeg",
+      "/images/boda2.jpeg",
+    ],
+    events: [
+      {
+        id: "event1",
+        time: "13:00 hrs.",
+        title: "Misa de Acción de Gracias",
+        description: "Ceremonia en la Parroquia Santa Fe de Guadalupe.",
+        icon: "⛪",
+      },
+      {
+        id: "event2",
+        time: "14:00 hrs.",
+        title: "Recepción en Salón Texano",
+        description: "Comida, baile y celebración en el Salón Texano.",
+        icon: "🎉",
+      },
+      {
+        id: "event3",
+        time: "15:00 hrs.",
+        title: "Brindis y Palabras",
+        description: "Brindis especial y palabras de agradecimiento.",
+        icon: "🥂",
+      },
+      {
+        id: "event4",
+        time: "16:00 hrs.",
+        title: "Primer Baile",
+        description: "Primer baile y apertura de la pista de baile.",
+        icon: "💃",
+      },
+      {
+        id: "event5",
+        time: "18:00 hrs.",
+        title: "Corte de Pastel",
+        description: "Momento especial del corte de pastel.",
+        icon: "🍰",
+      },
+      {
+        id: "event6",
+        time: "19:00 hrs.",
+        title: "Baile y Diversión",
+        description: "Música, baile y diversión para todos los invitados.",
+        icon: "🎶",
+      },
+      {
+        id: "event7",
+        time: "20:00 hrs.",
+        title: "Mariachi",
+        description: "Presentación especial de mariachi.",
+        icon: "🎺",
+      },
+      {
+        id: "event8",
+        time: "21:00 hrs.",
+        title: "Despedida y Agradecimientos",
+        description: "Palabras finales y despedida de los invitados.",
+        icon: "🙏",
+      },
+    ],
+  },
+  dressCode: {
+    title: "Código de Vestimenta",
+    message: "¡Vístete para impresionar!",
+    subtitle: "Código de vestimenta formal - Blanco solo la novia",
+    restriction: "Restricción: No niños",
+    backgroundImage: "/images/dressCode1.png",
+  },
+  countdown: {
+    targetDate: "December 27, 2025 17:00:00",
+    backgroundImage: "/images/countdown-bg.jpg",
+  },
+  attendance: {
+    whatsappNumber: "5214621489904", //+52 1 462 148 9904
+    title: "CONFIRMACIÓN DE ASISTENCIA",
+    message: "Respetuosamente",
+    subtitle: "Confirmar antes del evento.",
+    fields: {
+      name: "Nombre completo",
+      response: "¿Podrás acompañarme?",
+      companions: "Nombre(s) de acompañante(s)",
+      phone: "Número de celular",
+      responseOptions: {
+        yes: "¡Claro, ahí estaré!",
+        no: "Lo siento, no podré asistir.",
+      },
+    },
+    images: [
+      "/images/boda3.jpeg",
+      "/images/boda4.jpeg",
+      "/images/boda5.jpeg",
+      "/images/boda6.jpeg",
+    ],
+    thankYouMessage:
+      "¡Gracias por confirmar tu asistencia! Nos alegra que puedas acompañarnos en este día tan especial.",
+  },
+  gifts: {
+    title: "Lista de Regalos",
+    subtitle:
+      "Tu presencia es el mejor regalo, pero si deseas contribuir, aquí tienes algunas ideas.",
+    message:
+      "Agradecemos de corazón tu generosidad y apoyo en este día tan especial. ¡Gracias por ser parte de nuestra vida!",
+    giftsOptions: [
+      {
+        id: "lluviaSobres",
+        name: "Lluvia de Sobres",
+        icon: "💌",
+        description:
+          "Tu presencia es el mejor regalo, pero si deseas contribuir, una lluvia de sobres sería muy apreciada.",
+        image: "/images/gifts/envelope.png",
+        link: "https://example.com/lluvia-de-sobres",
+      },
+      {
+        id: "regaloSorpresa",
+        name: "Regalo Sorpresa",
+        icon: "🎁",
+        description:
+          "Si prefieres darnos un regalo físico, estaremos encantados de recibir una sorpresa especial.",
+        image: "/images/gifts/gift-box.png",
+        link: "https://example.com/mesa-de-regalos",
+      },
+    ],
+  },
+  gallery: {
+    title: "Recuerdos Especiales",
+    subtitle: "Momentos inolvidables",
+    description:
+      "Cada imagen captura la esencia de este día tan especial. ¡Gracias por ser parte de estos recuerdos inolvidables!",
+    images: [
+      {
+        id: "image1",
+        src: "/images/g&f-01.jpg",
+        alt: "Nuestra Boda",
+        caption: "Nuestro día especial.",
+      },
+      {
+        id: "image2",
+        src: "/images/g&f-02.jpg",
+        alt: "Baile de Boda",
+        caption: "El emotivo primer baile de Boda.",
+      },
+      {
+        id: "image3",
+        src: "/images/g&f-03.jpg",
+        alt: "Corte de Pastel",
+        caption: "El dulce momento del corte de pastel.",
+      },
+      {
+        id: "image4",
+        src: "/images/g&f-04.jpg",
+        alt: "Celebración con Familia y Amigos",
+        caption: "Rodeados de sus seres queridos.",
+      },
+      {
+        id: "image5",
+        src: "/images/g&f-05.jpg",
+        alt: "Detalles del Evento",
+        caption: "Cada detalle hecho con amor para este día especial.",
+      },
+      {
+        id: "image6",
+        src: "/images/g&f-06.jpg",
+        alt: "Diversión en la Pista de Baile",
+        caption: "Momentos de alegría y baile con todos los invitados.",
+      },
+      {
+        id: "image7",
+        src: "/images/g&f-07.jpg",
+        alt: "Sesión de Fotos",
+        caption: "Capturando recuerdos que durarán toda la vida.",
+      },
+      {
+        id: "image8",
+        src: "/images/g&f-08.jpg",
+        alt: "Los novios y sus Padres",
+        caption: "Un momento especial con sus queridos padres.",
+      },
+    ],
+    imagesUrls: [
+      "/images/rapunzel1.jpeg",
+      "/images/rapunzel2.jpeg",
+      "/images/rapunzel3.jpeg",
+      "/images/rapunzel4.jpeg",
+      "/images/rapunzel5.jpeg",
+      "/images/rapunzel6.jpeg",
+      "/images/rapunzel7.jpeg",
+      "/images/rapunzel8.jpeg",
+    ],
+  },
+  qrcodeSection: {
+    title: "Escanea el Código QR",
+    celebrant: "Laura & Edgar",
+    message: "Para acceder fácilmente a la invitación en tu dispositivo móvil.",
+    mainImage: "/images/qrcode-bg.jpg",
+  },
+  music: {
+    src: "/music/quinceanera-song.mp3",
+    title: "Canción de Quinceañera",
+    artist: "Artista Invitado",
+  },
+  // 🎵 Configuración de audio
+  audio: {
+    src: "/audio/musica.mp3",
+    fallbacks: ["/audio/musica.ogg", "/audio/musica.wav"],
+    title: "Música de Fondo de Boda",
+    startTime: 5, //  Donde empieza la letra
+    endTime: 200, //  Final del segmento
+    volume: 0.7, // % de volumen
+    loop: true, // Loop en el rango especificado
+    preload: "metadata", // Precargar solo metadatos
+    enabled: true, // Control habilitado
+    position: {
+      desktop: { bottom: "2rem", right: "2rem" },
+      mobile: { bottom: "1rem", right: "1rem" },
+    },
+    styling: {
+      size: {
+        desktop: "60px",
+        mobile: "50px",
+      },
+      colors: {
+        primary: "#e3aaaa",
+        hover: "#d48c8c",
+        background: "rgba(255, 255, 255, 0.8)",
+        icon: "#333",
+      },
+    },
+  },
+  VIP_COLORS: {
+    rosaAurora: "#D85251",
+    lavandaAurora: "#7B5C6E",
+    oroAurora: "#DA9A6A",
+    blancoSeda: "#FFFFFF",
+    cremaSuave: "#F2E1C3",
+    rosaIntensa: "#8D0327",
+    lavandaIntensa: "#822D5C",
+    oroIntensio: "#A66F2E",
+    rosaDelicada: "#E69779",
+  },
+  customInvitations: {
+    adminPassword: "admin1234",
+    invitationUrl: "https://boda-vip-new-demo.vercel.app/",
+    suggested_messages: [
+      "¡Querida amiga! Te invitamos a celebrar con nosotros el día más mágico de nuestra vida. ¡Espero verte brillar junto a nosotros!",
+      "¡Familia querida! Este día especial no sería lo mismo sin ustedes. ¡Los esperamos con mucho amor!",
+      "¡Hola! Nos encantaría que seas parte de nuestra celebración. ¡Será una noche inolvidable!",
+      "¡Queridos padrinos! Su presencia es fundamental en este momento tan especial. ¡Los esperamos con cariño!",
+      "¡Amigos del alma! Vengan a celebrar con nosotros esta nueva etapa. ¡Será una fiesta increíble!",
+    ],
+  },
+};
